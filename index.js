03c08bee36ecf59a7fff418b994d1187b80364c9
coderyourfuture <you@example.com>
2018-06-24 17:17:48 +0100

    run it

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Docker!"));
+app.get("/", (req, res) => res.send("Run Docker please!"));
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
